eed1359f3832be67b3b4b48e4c66857714e78a42
matematica-art <79476496+matematica-art@users.noreply.github.com>
2026-07-24 20:47:46 -0500

    Update conexion.js

--- backend/conexion.js
+++ backend/conexion.js
@@ -1,1 +1,38 @@
+// =====================================
+// Conexión Base de Datos LMS IA
+// =====================================
 
+const mysql = require("mysql2");
+
+
+// Configuración de conexión
+
+const conexion = mysql.createConnection({
+
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "lms_ia",
+    port: 3306
+
+});
+
+
+// Probar conexión
+
+conexion.connect((error) => {
+
+    if (error) {
+
+        console.log("Error conectando a la base de datos:", error);
+
+    } else {
+
+        console.log("Base de datos LMS IA conectada correctamente ✅");
+
+    }
+
+});
+
+
+module.exports = conexion;
